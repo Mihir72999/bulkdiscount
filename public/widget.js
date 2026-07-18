598de35c70ba7089feaf2fb8dbdb21439cf42dbb
Mihir72999 <mihir72999@gmail.com>
2026-07-18 21:46:50 +0530

    some changes

--- public/widget.js
+++ public/widget.js
@@ -415,8 +415,8 @@ function bindEvents() {
 
 console.log('discounttype',discountType)
   function calculatePrice(price, discount , type=discountType) {
-    const price = type === 'percent' ? Number(price - (price * discount / 100)) : Number(price - (price - discount))
-    return price;
+    const prices = type === 'percent' ? Number(price - (price * discount / 100)) : Number(price - (price - discount))
+    return prices;
 }
 function updateDisplayedPrice(discount, qty ) {
 
