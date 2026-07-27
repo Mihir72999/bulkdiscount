8add9328c17562cbd2fa5be5e20d09050e7c74c5
Mihir72999 <111626217+Mihir72999@users.noreply.github.com>
2026-07-27 15:16:43 +0530

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -54,8 +54,8 @@ import { HugeiconsIcon } from '@hugeicons/react';
 const Products = () => {
   
 return (
-  <div>
-    <Card className="p-6">
+  <div className="max-w-5xl mx-auto p-6 space-y-6">
+    <Card className="p-3">
     <div className="flex items-center justify-between">
 
         <div>
