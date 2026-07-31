2e898c256e088489136dc03415315ed3d68c4d6b
Mihir72999 <mihir72999@gmail.com>
2026-07-31 08:00:05 +0530

    some changes

--- public/widget.js
+++ public/widget.js
@@ -188,6 +188,7 @@ function renderColorSelectors(quantity) {
   `).join("");  
 }
 
+
 const hasVariantOptions = variant.some(v => v.option_values?.length > 0);
 console.log(variant , 'variant')
 console.log(hasVariantOptions , 'hasVariantOptions')
