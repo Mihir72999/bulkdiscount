dfe9d9bd170a43705e1e42968e281d094eaa9aef
Mihir72999 <mihir72999@gmail.com>
2026-08-03 08:32:07 +0530

    some changes in dynamic route

--- src/app/products/[pid]/page.tsx
+++ src/app/products/[pid]/page.tsx
@@ -15,6 +15,7 @@ const ProductInfo = () => {
    useEffect(()=>{
     if(setting.length > 0){
         const find = setting.filter((data:{id:string , name:string})=> data.id === pid)
+        console.log('find',find)
         setSetting(find ?? [])
     }
    },[setting , pid])
