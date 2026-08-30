6db87f5cd1b1ee4c48d9c91ee9456a64264dfa01
Mihir72999 <mihir72999@gmail.com>
2026-08-30 21:11:01 +0530

    some changes

--- src/lib/getsearchparams.ts
+++ src/lib/getsearchparams.ts
@@ -3,12 +3,13 @@ import { NextRequest } from "next/server";
 export default function getSearchParams(request:NextRequest , search:string | string[]){
  
  let searchParam:string = "";
- const searchParams:string[] = []
+ const searchParams:(string | null)[] = []
 
  if(typeof search === "string"){
     searchParam = request.nextUrl.searchParams.get(search) || ""
- }else{
-    searchParams.push(...search)
+ }else{      
+  const value =  search.length > 0 ? search.map(s => request.nextUrl.searchParams.get(s)) : []
+    searchParams.push(...value)
  }
 
 return typeof search === "string" ? searchParam : searchParams 
